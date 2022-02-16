ae485174e3209e27bbc6094bd667a1cf611173b2
shaolong feng <feng.shao@northeastern.edu>
2022-02-15 19:16:29 -0800

    Assignment 3-2

---
+++ As3-segue/As3-segue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  As3-segue
+//
+//  Created by user204690 on 2/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
